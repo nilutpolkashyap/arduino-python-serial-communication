0b2e415f441f59d766c6a8610ccf3462e9f93449
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2020-03-13 01:52:49 +0530

    Add files via upload

---
+++ serial_data_send.py
@@ -0,0 +1,16 @@
+import serial
+import time
+
+ser1 = serial.Serial('COM_PORT', 9600)
+ser1.timeout = 1
+
+while True:
+    i = input("input(on/off): ").strip()
+    if i =='done':
+        print('finished')
+        break
+    ser1.write(i.encode())
+    time.sleep(0.5)
+    #print(ser1.readline().decode('ascii'))
+
+ser1.close()
